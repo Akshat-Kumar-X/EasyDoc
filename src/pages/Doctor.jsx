66454059557911646f01718fe7902b59a2d41322
Akshat Kumar <akshatkumar10203@gmail.com>
2024-03-15 15:17:07 +0530

    edited blogs

--- src/pages/Doctor.jsx
+++ src/pages/Doctor.jsx
@@ -3,7 +3,6 @@ import SearchResults from '../components/SearchResults';
 import searchData from '../../public/data.js';
 import { IoLocationSharp } from "react-icons/io5";
 import { MdMedicalServices } from "react-icons/md";
-import { Link } from 'react-router-dom';
 
 const Doctor = () => {
   const [nameQuery, setNameQuery] = useState('');
